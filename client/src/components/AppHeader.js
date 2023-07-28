80d729d9ba8f0c0a364aa3bdb213aaf2d75dca66
Даниил Нечитайло <masterwars22@yandex.ru>
2023-07-28 17:45:13 +0300

    add
    side menu with styles, not finished yet

--- client/src/components/AppHeader.js
+++ client/src/components/AppHeader.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import '../styles/AppHeader.css';
-import { BurgerMenuIcon } from './Icons';
+import React, { useState } from "react";
+import "../styles/AppHeader.css";
+import { BurgerMenuIcon, CancelIcon } from "./Icons";
+import SideMenu from "./SideMenu";
 
 export default function AppHeader() {
+    const [isMenuOpened, setIsMenuOpened] = useState(false);
+    const changeMenuVisibility = () => {
+        setIsMenuOpened(!isMenuOpened);
+    };
     return (
         <div className='AppHeader'>
             <div className='AppHeader__title'>Awingen</div>
-            <div className='AppHeader__button'>
-                <BurgerMenuIcon size={40} color='#137C67'/>
+            <div className='AppHeader__button' onClick={changeMenuVisibility}>
+                {isMenuOpened ? (
+                    <CancelIcon size={40} color='#137C67' />
+                ) : (
+                    <BurgerMenuIcon size={40} color='#137C67' />
+                )}
+
+                {isMenuOpened && <SideMenu onClose={changeMenuVisibility} />}
             </div>
         </div>
-    )
+    );
 }
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,76 +1,73 @@
-import React, {useContext} from 'react';
-import {Context} from "../index";
+import React, { useContext } from "react";
+import { Context } from "../index";
 // import Navbar from "react-bootstrap/Navbar";
 // import Nav from "react-bootstrap/Nav";
-import {NavLink, Navigate} from "react-router-dom";
-import {Button} from "react-bootstrap";
-import {observer} from "mobx-react-lite";
+import { NavLink, Navigate } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import { observer } from "mobx-react-lite";
 import Container from "react-bootstrap/Container";
-import {useNavigate} from "react-router-dom"
-import './output.css'
+import { useNavigate } from "react-router-dom";
+import "./stylenav.css";
 
 const NavBar = observer(() => {
-    const activeLink = 'nav-list__link nav-list__link--active';
-	const normalLink = 'nav-list__link';
+    const activeLink = "nav-list__link nav-list__link--active";
+    const normalLink = "nav-list__link";
 
     return (
-        
+        <nav className='nav'>
+            <div className='container'>
+                <div className='nav-row'>
+                    <NavLink to='/' className='logo'>
+                        <strong>AWINGEN</strong>
+                    </NavLink>
 
-<nav className="nav">
-  <div className="container">
-      <div className="nav-row">
-          <NavLink to="/" className="logo">
-              <strong>AWINGEN</strong>
-          </NavLink>
+                    <ul className='nav-list'>
+                        <li className='nav-list__item'>
+                            <NavLink
+                                to='/about'
+                                className={({ isActive }) =>
+                                    isActive ? activeLink : normalLink
+                                }
+                            >
+                                About
+                            </NavLink>
+                        </li>
 
-
-          <ul className="nav-list">
-              <li className="nav-list__item">
-                  <NavLink
-                      to="/about"
-                      className={({ isActive }) =>
-                          isActive ? activeLink : normalLink
-                      }
-                  >
-                      About
-                  </NavLink>
-              </li>
-
-              <li className="nav-list__item">
-                  <NavLink
-                      to="/downloads"
-                      className={({ isActive }) =>
-                          isActive ? activeLink : normalLink
-                      }
-                  >
-                      Downloads
-                  </NavLink>
-              </li>
-              <li className="nav-list__item">
-                  <NavLink
-                      to="/keys"
-                      className={({ isActive }) =>
-                          isActive ? activeLink : normalLink
-                      }
-                  >
-                      Keyses
-                  </NavLink>
-              </li>
-              <li className="nav-list__item">
-                  <NavLink
-                      to="/videos"
-                      className={({ isActive }) =>
-                          isActive ? activeLink : normalLink
-                      }
-                      //onClick={() => logOut()}
-                  >
-                      Videos
-                  </NavLink>
-              </li>
-          </ul>
-      </div>
-  </div>
-</nav> 
+                        <li className='nav-list__item'>
+                            <NavLink
+                                to='/downloads'
+                                className={({ isActive }) =>
+                                    isActive ? activeLink : normalLink
+                                }
+                            >
+                                Downloads
+                            </NavLink>
+                        </li>
+                        <li className='nav-list__item'>
+                            <NavLink
+                                to='/keys'
+                                className={({ isActive }) =>
+                                    isActive ? activeLink : normalLink
+                                }
+                            >
+                                Keyses
+                            </NavLink>
+                        </li>
+                        <li className='nav-list__item'>
+                            <NavLink
+                                to='/videos'
+                                className={({ isActive }) =>
+                                    isActive ? activeLink : normalLink
+                                }
+                                //onClick={() => logOut()}
+                            >
+                                Videos
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
 });
 
