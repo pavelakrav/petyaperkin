dd2759d751e1f827ff6a423049cf7d3bdfae5140
Даниил Нечитайло <masterwars22@yandex.ru>
2023-08-02 21:29:38 +0300

    upd
    video wrapper style correct now without scrolls

--- client/src/pages/main.js
+++ client/src/pages/main.js
@@ -1,26 +1,30 @@
-import React, { useContext } from "react";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import { NavLink, Navigate } from "react-router-dom";
-import { Button } from "react-bootstrap";
-import { observer } from "mobx-react-lite";
-import Container from "react-bootstrap/Container";
-import { useNavigate } from "react-router-dom";
+import React, { useContext } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { NavLink, Navigate } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import { observer } from 'mobx-react-lite';
+import Container from 'react-bootstrap/Container';
+import { useNavigate } from 'react-router-dom';
 import Awingen_Trim from './Awingen_Trim.mp4';
-import "../styles/videos.css"
-
+import '../styles/videos.css';
 
 const main = observer(() => {
-    const activeLink = "nav-list__link nav-list__link--active";
-    const normalLink = "nav-list__link";
+    const activeLink = 'nav-list__link nav-list__link--active';
+    const normalLink = 'nav-list__link';
 
-    return  (<><video className='videoTag' autoPlay loop>
-    <source src={Awingen_Trim} type='video/mp4' />
-    </video>
-    <div id = "fashion">
-      <h2><p>AUTONOMOUS</p>WIND GENERATORS</h2>
-    </div></>) 
-
+    return (
+        <>
+            <video className="videoTag" autoPlay loop>
+                <source src={Awingen_Trim} type="video/mp4" />
+            </video>
+            <div id="fashion">
+                <h2>
+                    <p>AUTONOMOUS</p>WIND GENERATORS
+                </h2>
+            </div>
+        </>
+    );
 });
 
 export default main;
