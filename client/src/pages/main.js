0db87cd5bbde7dc26b617a79c28d9be8bd327bb6
pavelakrav <135419663+pavelakrav@users.noreply.github.com>
2023-08-02 20:21:33 +0300

    Pasha
    Added main.js

--- client/src/pages/main.js
+++ client/src/pages/main.js
@@ -6,12 +6,21 @@ import { Button } from "react-bootstrap";
 import { observer } from "mobx-react-lite";
 import Container from "react-bootstrap/Container";
 import { useNavigate } from "react-router-dom";
+import Awingen_Trim from './Awingen_Trim.mp4';
+import "../styles/videos.css"
+
 
 const main = observer(() => {
     const activeLink = "nav-list__link nav-list__link--active";
     const normalLink = "nav-list__link";
 
-    return <div>Main</div>;
+    return  (<><video className='videoTag' autoPlay loop>
+    <source src={Awingen_Trim} type='video/mp4' />
+    </video>
+    <div id = "fashion">
+      <h2><p>AUTONOMOUS</p>WIND GENERATORS</h2>
+    </div></>) 
+
 });
 
 export default main;
