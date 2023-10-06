968e5101d4fd0c40af16828f482e6ef7b35f4b07
pavelakrav <135419663+pavelakrav@users.noreply.github.com>
2023-10-06 19:02:10 +0300

    added photos
    ll

--- client/src/pages/cases.jsx
+++ client/src/pages/cases.jsx
@@ -1,8 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { observer } from 'mobx-react-lite';
+//import '../styles/downloads.css';
+import Modal from '../components/Modal';
+import styles from '../styles/cardVideos.module.css';
+import playBtn from '../images/play.png';
+import Awingen_Prewiev from '../images/Awingen_Preview.png';
+import Background from '../images/background_Beauty.jpg'
+import Cliffs from '../images/Cliffs.jpg'
 
 const cases = observer(() => {
-	return <></>;
+	const [isModalVisible, setIsModalVisible] = useState(false);
+	const onClickHandler = (event, name) => {
+		setIsModalVisible(!isModalVisible);
+	};
+	const CardContent = [
+		{ title: 'Video1' },
+		{ title: 'Video2' },
+		{ title: 'Video3' },
+		{ title: 'Video4' },
+	];
+
+	return (
+		<>
+			<div className={styles.cardsListVideos}>
+				{CardContent.map((content) => {
+					return (
+						<>
+
+
+							<div className={styles.cardVideos}>
+								<img
+									src={Awingen_Prewiev}
+									className={styles.smallVideo}
+									alt="pic"
+								/>
+									<a 
+										className={styles.play_btn}
+										onClick={onClickHandler}
+									>
+										<img
+											src={playBtn}
+											className={styles.play_btn}
+											alt="pic"
+										/>
+									</a>
+							</div>
+							
+						</>
+					);
+				})}
+			</div>
+			{isModalVisible && (
+				<Modal type={'Modal'} onClose={onClickHandler}>
+					<div
+						className={styles.VideoWrapper}
+						onClick={(event) => {
+							event.stopPropagation();
+						}}
+					></div>
+				</Modal>
+			)}
+		</>
+	);
 });
 
 export default cases;
